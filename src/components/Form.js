63c14da38e12b5dbe04a7efe4885e3e9d903b5eb
Aas11 <aas111.hope@gmail.com>
2024-01-10 19:17:44 +0530

    code changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -16,26 +16,30 @@ function Form({ onAddItems }) {
   }
 
   return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😶 trip ? </h3>
-      <select
-        value={quantity}
-        onChange={(e) => Number(setQuantity(e.target.value))}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item..."
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <button>Add</button>
-    </form>
+    <>
+      <form className="add-form" onSubmit={handleSubmit}>
+        <h3>What do you need for your 😶 trip ? </h3>
+        <div className="select-input">
+          <select
+            value={quantity}
+            onChange={(e) => Number(setQuantity(e.target.value))}
+          >
+            {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+              <option value={num} key={num}>
+                {num}
+              </option>
+            ))}
+          </select>
+          <input
+            type="text"
+            placeholder="Item..."
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <button>Add</button>
+      </form>
+    </>
   );
 }
 export default Form;
